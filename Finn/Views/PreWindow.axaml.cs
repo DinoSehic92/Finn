4ee92af68cdac6d4e37a4181a9ba9f06da2a567d
DOSC <dino.sehic@ramboll.se>
2026-05-05 13:32:09 +0200

    Bugfix on hotkeys

--- Finn/Views/PreWindow.axaml.cs
+++ Finn/Views/PreWindow.axaml.cs
@@ -11,7 +11,7 @@ public partial class PreWindow : Window
     {
         InitializeComponent();
 
-        KeyDown += CloseKey;
+        KeyDown += OnWindowKeyDown;
     }
 
     protected override void OnClosing(WindowClosingEventArgs e)
@@ -26,11 +26,29 @@ public partial class PreWindow : Window
         e.Cancel = false;
     }
 
-    private void CloseKey(object? sender, KeyEventArgs e)
+    private void OnWindowKeyDown(object? sender, KeyEventArgs e)
     {
         if (e.Key == Key.Escape)
         {
             this.Close();
+            return;
+        }
+
+        if (this.DataContext is not MainViewModel ctx) return;
+
+        // Forward Left/Right arrow navigation at window level so page switching
+        // works regardless of which child element currently holds focus.
+        if (e.Key == Key.Left && e.KeyModifiers == KeyModifiers.None)
+        {
+            if (ctx.PreviewVM.PrevPageCommand.CanExecute(false))
+                ctx.PreviewVM.PrevPageCommand.Execute(false);
+            e.Handled = true;
+        }
+        else if (e.Key == Key.Right && e.KeyModifiers == KeyModifiers.None)
+        {
+            if (ctx.PreviewVM.NextPageCommand.CanExecute(false))
+                ctx.PreviewVM.NextPageCommand.Execute(false);
+            e.Handled = true;
         }
     }
 
